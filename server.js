27f2a5129bc1bd65f4dd5b60b8229167e182cb2c
정대영 <rnt200@naver.com>
2023-11-10 23:36:59 +0900

    1

---
+++ server.js
@@ -0,0 +1,41 @@
+const http = require('http');
+var express = require('express');
+const socketIo = require('socket.io');
+const cors = require('cors');
+const PORT=3000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server,{
+  cors:{
+    origin:'*',
+    method:['GET','POST']
+  }
+});
+
+
+io.on('connection', (socket) => {
+
+    console.log('server has started!');
+  
+
+    var toRoom;
+    
+    socket.on('join',(data)=>{
+      console.log(`${socket.id} 가 ${data} room 에 입장`);
+      toRoom=data;
+      socket.join(data);
+     
+    });
+
+
+    socket.on('message', (data) => {
+      console.log('reicevied', data);
+      io.to(toRoom).emit('message', data); // send all client
+    });
+});
+  
+
+server.listen(PORT, () => {
+  console.log(`server is running at ${PORT} port`);
+});
